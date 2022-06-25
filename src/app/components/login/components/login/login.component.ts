c9a3de922016ce0388020bf3e70242a24f7fcd60
granadalook <tiven17.jsga@gmail.com>
2022-06-25 14:09:52 -0500

    CREATE: Se crea el servicio toast.service y modulos toast.component y toaster.component  con sus interfaces para uso  y despliegue de mensajes de alerta

--- src/app/components/login/components/login/login.component.ts
+++ src/app/components/login/components/login/login.component.ts
@@ -5,6 +5,8 @@ import {
   FormGroup,
   Validators,
 } from '@angular/forms';
+import { ToastService } from 'src/app/core/services/Toast/toast.service';
+import { EventTypes } from 'src/app/models/event-types';
 import { LoginUser } from 'src/app/models/loginUser';
 
 @Component({
@@ -13,10 +15,10 @@ import { LoginUser } from 'src/app/models/loginUser';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  
   email?: string;
   password?: string;
   loginUser: LoginUser;
+  EventTypes = EventTypes;
 
   public formularioLogin: FormGroup = this.formBuilder.group({
     email: ['', [Validators.email, Validators.required]],
@@ -26,18 +28,52 @@ export class LoginComponent implements OnInit {
     ],
   });
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private toastService: ToastService
+  ) {
     this.loginUser = {
       email: '',
       password: '',
     };
   }
   ngOnInit(): void {}
- 
+  showToast(type: EventTypes) {
+    switch (type) {
+      case EventTypes.Success:
+        this.toastService.showSuccessToast(
+          'Success toast title',
+          'This is a success toast message.'
+        );
+        break;
+      case EventTypes.Warning:
+        this.toastService.showWarningToast(
+          'Warning toast title',
+          'This is a warning toast message.'
+        );
+        break;
+      case EventTypes.Error:
+        this.toastService.showErrorToast(
+          'VAlORES INCORRECTOS',
+          'INTENTE DE NUEVO IMBESIL'
+        );
+        break;
+      default:
+        this.toastService.showInfoToast(
+          'Info toast title',
+          'This is an info toast message.'
+        );
+        break;
+    }
+  }
 
   enviarFormulario() {
+    this.showToast(EventTypes.Success);
     console.log(this.loginUser);
-    this.loginUser.email = '';
-    this.loginUser.password = '';
+    setTimeout(() => {
+      console.log(this.loginUser);
+      this.loginUser.email = '';
+      this.loginUser.password = '';
+    }, 2500);
   }
 }
