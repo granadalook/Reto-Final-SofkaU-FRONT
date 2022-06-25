d61dccf001532cb25356f175175055db42928ae1
granadalook <tiven17.jsga@gmail.com>
2022-06-25 13:07:51 -0500

    CREATE. se crea login user formulario  con validaciones

--- src/app/components/login/components/login/login.component.ts
+++ src/app/components/login/components/login/login.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  EmailValidator,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { LoginUser } from 'src/app/models/loginUser';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  
+  email?: string;
+  password?: string;
+  loginUser: LoginUser;
 
-  constructor() { }
+  public formularioLogin: FormGroup = this.formBuilder.group({
+    email: ['', [Validators.email, Validators.required]],
+    password: [
+      '',
+      [Validators.required, Validators.minLength(5), Validators.maxLength(8)],
+    ],
+  });
 
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder) {
+    this.loginUser = {
+      email: '',
+      password: '',
+    };
   }
+  ngOnInit(): void {}
+ 
 
+  enviarFormulario() {
+    console.log(this.loginUser);
+    this.loginUser.email = '';
+    this.loginUser.password = '';
+  }
 }
