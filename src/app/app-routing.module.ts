efd87760721156a6d56ed3663bc193d03422df05
granadalook <tiven17.jsga@gmail.com>
2022-06-24 19:36:31 -0500

    FIRST COMMIT:Se inicializa proyecto de front  con angular    creando rutas con lazzy loading  modulo compartido de shared  y modulos de registro homepage y login  incorporando bootstrap  con dependencias de Jquery y  funentes de  google fonts

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./components/homepage/homepage.module').then(
+        (modulo) => modulo.HomepageModule
+      ),
+  },
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./components/login/login.module').then(
+        (modulo) => modulo.LoginModule
+      ),
+  },
+  {
+    path: 'registro',
+    loadChildren: () =>
+      import('./components/registro/registro.module').then(
+        (modulo) => modulo.RegistroModule
+      ),
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
