80ed455d92a4787d62b13e4cc5c487bf98a18581
granadalook <tiven17.jsga@gmail.com>
2022-06-26 13:42:20 -0500

    CREATE:  Se hace llamado al back  usando metodo post usando headers  creando un  metodo headers

--- src/app/core/services/Autenticacion/autenticacion.service.ts
+++ src/app/core/services/Autenticacion/autenticacion.service.ts
@@ -1,11 +1,33 @@
 import { Injectable } from '@angular/core';
+import { LoginUser } from 'src/app/models/loginUser';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { UsuarioI } from 'src/app/models/usuario';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AutenticacionService {
-
-  constructor() { }
-
-  
+  constructor(private http: HttpClient) {}
+  sendHeaders() {
+    let headers = new HttpHeaders();
+    headers = headers.append('content-type', 'application/json');
+    headers = headers.append('Access-Control-Allow-Origin', '*');
+    headers = headers.append(
+      'Access-Control-Allow-Headers',
+      'Origin, Content-Type, X-Auth-Token'
+    );
+    headers = headers.append(
+      'Access-Control-Allow-Methods',
+      'GET,POST,OPTIONS,DELETE,PUT'
+    );
+    return headers;
+  }
+  loginUserPassword(usuarioenviado: LoginUser) {
+    return this.http.post<UsuarioI>(
+      `${environment.UrlBase}${environment.Login}`,
+      usuarioenviado,
+      { headers: this.sendHeaders() }
+    );
+  }
 }
