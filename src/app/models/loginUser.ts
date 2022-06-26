e3dbb4722dfb7382ece6edecb2c01e4d07914fc1
granadalook <tiven17.jsga@gmail.com>
2022-06-26 13:45:46 -0500

    FIX  se ponen los campos de nombre completo y rol   hasta que se arregle el back

--- src/app/models/loginUser.ts
+++ src/app/models/loginUser.ts
@@ -1,4 +1,6 @@
 export interface LoginUser {
   email: string;
   password: string;
+  nombreCompleto: string; //  provisional hasta que lo arreglen
+  rol: string; //  provisional hasta que lo arreglen
 }
