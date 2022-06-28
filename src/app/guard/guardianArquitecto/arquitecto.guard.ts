6a1f794674d8cce3c43d458b86c6ce57b734a5f7
granadalook <tiven17.jsga@gmail.com>
2022-06-28 14:08:08 -0500

    FIX:Se crean componente guardianes para proteger las rutas de acuerdo a su rol

---
+++ src/app/guard/guardianArquitecto/arquitecto.guard.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  Router,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { AutenticacionService } from 'src/app/core/services/Autenticacion/autenticacion.service';
+import { ToastService } from 'src/app/core/services/Toast/toast.service';
+import { UsuarioI } from 'src/app/models/usuario';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ArquitectoGuard implements CanActivate {
+  user$?: UsuarioI;
+  constructor(
+    private autenticacionService: AutenticacionService,
+    private router: Router,
+    private toastService: ToastService
+  ) {}
+  canActivate():
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    this.autenticacionService.user$.subscribe((res) => (this.user$ = res));
+    console.log('this.user$.rol', this.user$?.rol);
+    if (this.user$?.rol === 'Arquitecto') {
+      return true;
+    }
+    this.toastService.showWarningToast(
+      'lo sentimos',
+      'Disponible solo a arquitectos'
+    );
+    this.router.navigate(['login']);
+    return true;
+  }
+}
