ca8da98c92ca4d956c33c5681ca72c84ba04c970
RodrigoDEVS <rodrigo.gallego@outlook.es>
2022-06-30 22:26:54 -0500

    UPDATE: Agregado el método para ocultar el botón de crear proyecto para el lider tecnico

--- src/app/guard/guardianLider/guardian-lider.guard.spec.ts
+++ src/app/guard/guardianLider/guardian-lider.guard.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { LiderGuard } from './lider.guard';
+import { GuardianLiderGuard } from './guardian-lider.guard';
 
-describe('LiderGuard', () => {
-  let guard: LiderGuard;
+describe('GuardianLiderGuard', () => {
+  let guard: GuardianLiderGuard;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    guard = TestBed.inject(LiderGuard);
+    guard = TestBed.inject(GuardianLiderGuard);
   });
 
   it('should be created', () => {
